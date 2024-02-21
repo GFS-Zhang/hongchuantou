cec275c02e77a53a183032fb7bdbdab09a7f17ba
zhanghaojie <13051323785@163.com>
2024-02-21 16:48:32 +0800

    初始化

---
+++ src/main/java/com/hct/HongchuantouApplication.java
@@ -0,0 +1,13 @@
+package com.hct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongchuantouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HongchuantouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hct/HongchuantouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongchuantouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
